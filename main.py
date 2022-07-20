ebaa549e282db1d39c786fd751041eebb648e51f
Marlon Dominguez <marlon.dominguez307@gmail.com>
2022-07-20 22:02:55 +0000

    we did alot

--- main.py
+++ main.py
@@ -20,6 +20,7 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 
 db = SQLAlchemy(app)
 
+
 #User Object For Table 
 class users(db.Model):
   id = db.Column(db.Integer, primary_key=True)
@@ -37,23 +38,6 @@ class users(db.Model):
   def __repr__(self):
     return f"users('{self.username}', '{self.email}')"
 
-#Table for matches
-
-
-#Table for Questions
-class prompt(db.Model):
-    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    Q1 = db.Column(db.String(20), nullable=True)
-    Q2 = db.Column(db.String(20), nullable=True)
-    Q3 = db.Column(db.String(20), nullable=True)
-    Q4 = db.Column(db.String(20), nullable=True)
-    Q5 = db.Column(db.String(20), nullable=True)
-
-    def __repr__(self):
-        return f"prompt('{self.Q1}', '{self.Q2}', '{self.Q3}', '{self.Q4}', '{self.Q5}')" 
-
-    
-    
 
 @app.route("/")
 def hello_world():
@@ -69,7 +53,7 @@ def second_page():
 def register():
     form = RegistrationForm()
     if form.validate_on_submit(): # checks if entries are valid
-        user = users(username=form.username.data, email=form.email.data, password=form.password.data, Q1=None, Q2=None, Q3=None, Q4=None, Q5=None)
+        user = users(username=form.username.data, email=form.email.data, password=form.password.data, Q1='none', Q2='none', Q3='None', Q4='None', Q5='None')
         db.session.add(user)
         db.session.commit()
         flash(f'Account created for {form.username.data}!', 'success')
@@ -94,37 +78,76 @@ def register():
 @app.route("/login", methods=['GET', 'POST'])
 def login():
     form = RegistrationForm2()
+
     if form.validate_on_submit(): # checks if entries are valid
+
         user = users(username=form.username.data, password=form.password.data)
+
         db.session.add(user)
+
         db.session.commit()
+
         flash(f'Account created for {form.username.data}!', 'success')
+
         return redirect(url_for('home')) # if so - send to home page
-    return render_template('login.html', title='Login', form=form) 
+
+    return render_template('login.html', title='Login', form=form)
+
 
 @app.route("/questionnaire", methods=['GET', 'POST'])
 def questions():
     return render_template('questionnaire.html', subtitle='Questionnaire Page', text='This is the questionnaire page')
+
+
+
 
 #Where questionnaire data is actually added to the database. Linked here from submit button in questionnaire.html, 
 #action="/questionnaire-result"
 @app.route("/questionnaire-result", methods=['POST'])
 def results():
     answer_1 = request.form.get('astrology')
+
     answer_2 = request.form.get('t.a')
+
     answer_3 = request.form.get('juice')
+
     answer_4 = request.form.get('planets')
+
     answer_5 = request.form.get('money')
     
-    rows = session.query(users).count()
-    print(rows)
-    #name = form.username.data, Q1 = answer_1 , Q2 = answer_2 , Q3 = answer_3 , Q4 = answer_4, Q5 = answer_5)
+  
+    connection = sqlite3.connect("site.db")
+
+    cur = connection.cursor()
+
+    #fetchall() iterator to get all rows in database
+    cur.execute("SELECT id FROM users ORDER BY id DESC LIMIT 1")
+
+    list = cur.fetchall()
+
+    users_size = list[0][0]
     
-    db.session.add(user_answers)
-    db.session.commit()
+    cur.execute("UPDATE users SET Q1 = '{}' WHERE ID = {};".format(answer_1, users_size))
+
+    cur.execute("UPDATE users SET Q2 = '{}' WHERE ID = {};".format(answer_2, users_size))
+
+    cur.execute("UPDATE users SET Q3 = '{}' WHERE ID = {};".format(answer_3, users_size))
+
+    cur.execute("UPDATE users SET Q4 = '{}' WHERE ID = {};".format(answer_4, users_size))
+
+    cur.execute("UPDATE users SET Q5 = '{}' WHERE ID = {};".format(answer_5, users_size))
     
+    #print(users_size)
+
+    connection.commit()
+
+    connection.close()
+
     #send the user back home when they submit the questionnaire. Can be changed to another page
-    return render_template('home.html', subtitle='Home Page', text='This is the home page')
+    return render_template('match.html', subtitle='Home Page', text='This is the home page')
+
+
+
 
 @app.route("/match", methods=['GET', 'POST'])
 def match():
@@ -132,10 +155,19 @@ def match():
     #query questionnaire
     return render_template('match.html', subtitle='Match Page', text='Welcome to the real Fun!')
 
+
+
+
 '''@app.route("/match-result", methods=['POST'])
 def matchresults():
     pass'''
 
+
+
+
 if __name__ == '__main__':
     #db.create_all()
     app.run(debug=True, host="0.0.0.0")
+
+
+
