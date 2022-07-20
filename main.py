4458ac0c3d7c0d59827df667b981a962c81f5c87
Caleb Harrigan <caleb.b.harrigan@vanderbilt.edu>
2022-07-20 18:52:19 +0000

    getting closer

--- main.py
+++ main.py
@@ -4,7 +4,9 @@ from flask_behind_proxy import FlaskBehindProxy
 from flask_sqlalchemy import SQLAlchemy
 from flask import Flask, render_template, url_for
 import random, threading, webbrowser
-import time 
+from sqlalchemy import create_engine, Table, Column, Integer, String, MetaData, insert, update
+import sqlite3
+import os
 
 print("Hello world!")
 
@@ -25,6 +27,12 @@ class users(db.Model):
   email = db.Column(db.String(120), unique=True, nullable=False)
   password = db.Column(db.String(60), nullable=False)
   #age = db.Column(db.String(60), nullable = False)
+  Q1 = db.Column(db.String(20), nullable=True)
+  Q2 = db.Column(db.String(20), nullable=True)
+  Q3 = db.Column(db.String(20), nullable=True)
+  Q4 = db.Column(db.String(20), nullable=True)
+  Q5 = db.Column(db.String(20), nullable=True)
+
 
   def __repr__(self):
     return f"users('{self.username}', '{self.email}')"
@@ -61,13 +69,24 @@ def second_page():
 def register():
     form = RegistrationForm()
     if form.validate_on_submit(): # checks if entries are valid
-        user = users(username=form.username.data, email=form.email.data, password=form.password.data)
+        user = users(username=form.username.data, email=form.email.data, password=form.password.data, Q1=None, Q2=None, Q3=None, Q4=None, Q5=None)
         db.session.add(user)
         db.session.commit()
         flash(f'Account created for {form.username.data}!', 'success')
+        
+        #making each users match table
+        name = form.username.data
+        engine = create_engine('sqlite:///site.db', echo = True)
+        meta = MetaData()
+        students = Table(
+        name, meta, 
+        Column('id', Integer, primary_key = True), 
+        Column('matches', String)
+        )
+        meta.create_all(engine)
+
         return redirect('/questionnaire') # if so - send to questionnaire
-        
-        
+
     return render_template('register.html', title='Register', form=form)
 
 
@@ -97,8 +116,9 @@ def results():
     answer_4 = request.form.get('planets')
     answer_5 = request.form.get('money')
     
-
-    user_answers = prompt(Q1 = answer_1 , Q2 = answer_2 , Q3 = answer_3 , Q4 = answer_4, Q5 = answer_5)
+    rows = session.query(users).count()
+    print(rows)
+    #name = form.username.data, Q1 = answer_1 , Q2 = answer_2 , Q3 = answer_3 , Q4 = answer_4, Q5 = answer_5)
     
     db.session.add(user_answers)
     db.session.commit()
@@ -106,6 +126,15 @@ def results():
     #send the user back home when they submit the questionnaire. Can be changed to another page
     return render_template('home.html', subtitle='Home Page', text='This is the home page')
 
+@app.route("/match", methods=['GET', 'POST'])
+def match():
+    #query username
+    #query questionnaire
+    return render_template('match.html', subtitle='Match Page', text='Welcome to the real Fun!')
+
+'''@app.route("/match-result", methods=['POST'])
+def matchresults():
+    pass'''
 
 if __name__ == '__main__':
     #db.create_all()
